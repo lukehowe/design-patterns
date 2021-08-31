b0c3feec44ac12dbc1153b8dbe9c48a10c2cbc4c
Luke Howe <lahowe@email.sc.edu>
2021-08-31 17:42:50 -0400

    Added variables, constructor, and required methods for BestSellers class.

--- observer/BestSellers.java
+++ observer/BestSellers.java
@@ -4,9 +4,35 @@
 
   // Import packages
 package observer;
+import java.util.ArrayList;
 
 public class BestSellers {
     
     // Create instance variables
-    
+    private ArrayList<Observer> observers;
+    private ArrayList<Book> bestSellers;
+
+    /**
+     * BestSellers constructor
+     */
+    public BestSellers() {
+        observers = new ArrayList<Observer>();
+        bestSellers = new ArrayList<Book>();
+    }
+
+    public void registerObserver(Observer observer) {
+
+    }
+
+    public void removeObserver(Observer observer) {
+
+    }
+
+    public void notifyObservers(Book book) {
+
+    }
+
+    public void addBook (Book book) {
+        
+    }
 }
