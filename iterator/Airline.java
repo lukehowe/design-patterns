c94e89d63f55f5b857d85b6fe63e67c768031498
Luke Howe <lahowe@email.sc.edu>
2021-09-21 23:55:45 -0400

    Added instance variables and empty methods for Airline.java

--- iterator/Airline.java
+++ iterator/Airline.java
@@ -6,4 +6,33 @@ package iterator;
 
 public class Airline {
     
+    // Create instance variables
+    private String title;
+    private Flight[] flights;
+    private int size;
+
+    // Create constructor
+
+    public Airline(String title) {
+
+    }
+
+    public void addFlight(String flightNum, String from, String to, int duration, int transfers) {
+
+    }
+
+    public String getTitle() {
+
+    }
+
+    private Flight[] growArray(Flight[] flights) {
+
+    }
+
+    public FlightIterator createIterator() {
+        
+    }
+
+
+
 }
