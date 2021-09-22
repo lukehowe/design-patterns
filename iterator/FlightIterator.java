eb49aa98587c7b82da5cb30f2c92ddb1c5fe252b
Luke Howe <lahowe@email.sc.edu>
2021-09-21 23:58:40 -0400

    Added instance variables and empty methods for FlightIterator.java

--- iterator/FlightIterator.java
+++ iterator/FlightIterator.java
@@ -6,4 +6,23 @@ package iterator;
 
 public class FlightIterator implements Iterator {
     
+    // Create instance variables
+
+    private Flight[] flights;
+    private int position;
+
+    // Create constructor
+
+    public FlightIterator(Flight[] flights) {
+
+    }
+
+    public boolean hasNext() {
+
+    }
+
+    public Flight next() {
+        
+    }
+
 }
