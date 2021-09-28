453bb56d04357ada107c18930d1b1361575e8dfe
Luke Howe <lahowe@email.sc.edu>
2021-09-28 12:44:25 -0400

    Added TriviaGame.java and required methods

--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,5 +1,37 @@
 package singleton;
 
+// Import packages
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * @author Luke Howe
+ */
+
 public class TriviaGame {
-    
+
+    // Create instance variables
+    private static TriviaGame triviaGame;
+    private int score;
+    private Random rand;
+    private Scanner reader;
+    private ArrayList<Question> questions;
+
+    // Create constructor
+    public TriviaGame() {
+
+    }
+
+    public static TriviaGame getInstance() {
+
+    }
+
+    public void play() {
+
+    }
+
+    private boolean playRound() {
+        
+    }
 }
