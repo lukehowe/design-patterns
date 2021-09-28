92781f1c625e011a1a90a6277d53ca931177821d
Luke Howe <lahowe@email.sc.edu>
2021-09-28 17:19:23 -0400

    Added TriviaGame private constructor, getInstance, play, and playRound.

--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -19,19 +19,44 @@ public class TriviaGame {
     private ArrayList<Question> questions;
 
     // Create constructor
-    public TriviaGame() {
-
+    private TriviaGame() {
+        questions = DataLoader.getTriviaQuestions();
+        Random rand = new Random();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Welcome to the Trivia Game!");
+        playRound();
     }
 
     public static TriviaGame getInstance() {
-
+        if(triviaGame == null) {
+            triviaGame = new TriviaGame();
+        }
+        return triviaGame;
     }
 
     public void play() {
-
+        boolean quitGame = false;
+        while(!quitGame) {
+            //playRound();
+            System.out.print("(P)lay or (Q)uit?:");
+            String response = reader.next();
+            if(response.toUpperCase() == "P") {
+                playRound();
+            }
+            else if(response.toUpperCase() == "Q") {
+                System.out.println("You won "+score+" games!\nGoodbye");
+                System.exit(0);
+            }
+            else {
+                return;
+            }
+        }
     }
 
     private boolean playRound() {
-        
+        int r = rand.nextInt(11);
+        questions.get(r).toString();
+        System.out.println("Enter Answer: ");
+        String response = reader.next();
     }
 }
