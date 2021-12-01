f42f785612f004556a282bdab01d1c2ad5f32fc9
Luke Howe <lahowe@email.sc.edu>
2021-12-01 17:52:43 -0500

    Added stubs

--- adapter/MedicalRecordAdapter.java
+++ adapter/MedicalRecordAdapter.java
@@ -1,5 +1,48 @@
 package adapter;
 
+// Import packages
+import java.util.Date;
+import java.util.ArrayList;
+/**
+ * @author Luke Howe
+ */
+
 public class MedicalRecordAdapter {
+
+    // Create instance variables
+    private HealthRecord record;
+
+    // Create constructor
+    public MedicalRecordAdapter(HealthRecord record) {
+
+    }
+
+    public String getFirstName() {
+        return "";
+    }
+
+    public String getLastName() {
+        return "";
+    }
     
+    public Date getBirthday() {
+        //TODO return date
+    }
+
+    public Gender getGender() {
+         //TODO return gender
+    }
+
+    public void addVisit(Date date, boolean well, String description) {
+
+    }
+
+    public ArrayList<Visit> getVisitHistory() {
+        //TODO return Visit history
+    }
+
+    public String toString() {
+        return "";
+    }
+
 }
