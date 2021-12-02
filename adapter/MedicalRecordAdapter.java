6808eee573d92dc3e63f226fc62d67e1dae88080
Luke Howe <lahowe@email.sc.edu>
2021-12-01 19:50:59 -0500

    some changes

--- adapter/MedicalRecordAdapter.java
+++ adapter/MedicalRecordAdapter.java
@@ -7,34 +7,43 @@ import java.util.ArrayList;
  * @author Luke Howe
  */
 
-public class MedicalRecordAdapter {
+public class MedicalRecordAdapter implements MedicalRecord {
 
     // Create instance variables
     private HealthRecord record;
 
     // Create constructor
     public MedicalRecordAdapter(HealthRecord record) {
-
+        this.record = record;
     }
 
     public String getFirstName() {
-        return "";
+        String[] split = record.getName().split(" ");
+        String firstName = split[0];
+        return firstName;
     }
 
     public String getLastName() {
-        return "";
+        String[] split = record.getName().split(" ");
+        String lastName = split[1];
+        return lastName;
     }
     
     public Date getBirthday() {
-        //TODO return date
+        return record.getBirthdate();
     }
 
     public Gender getGender() {
-         //TODO return gender
+        if(record.getGender().contains("male")) {
+            return Gender.MALE;
+        }
+        return Gender.FEMALE;
     }
 
     public void addVisit(Date date, boolean well, String description) {
-
+        //Visit visit;
+        //visit = new Visit(date, well, description);
+        
     }
 
     public ArrayList<Visit> getVisitHistory() {
